d632af2c55a9a84e9936908d2201e731b95c4991
ViniciusDev <viniciusdev10@gmail.com>
2024-12-30 10:27:44 -0300

    created css e form of aplication

--- cadastro usuarios/src/pages/home/index.jsx
+++ cadastro usuarios/src/pages/home/index.jsx
@@ -1,15 +1,51 @@
-import './style.css'
+import "./style.css";
+import Trash from "../../assets/trash.png";
 
 function Home() {
+  const users = [
+    {
+      id: "2faf4a5f44f54",
+      name: "Vinicius",
+      age: 33,
+      email: "vini@gmail.com",
+    },
+    { id: "51fafaf54f", 
+      name: "Maria", 
+      age: 13, 
+      email: "maria@gmail.com" },
+      { id: "hdjgj154jg45f5", 
+        name: "Heloise", 
+        age: 20, 
+        email: "helo@gmail.com" }
+      
+  ];
 
   return (
-    
-      <div>
-          <h1>Ola react</h1>
-      </div>
-  
-    
-  )
+    <div className="containe">
+      <form action="">
+        <h1>Cadasto de usuarios</h1>
+        <input type="text" name="nome" placeholder="Nome" />
+        <input type="number" name="idade" placeholder="Numero" />
+        <input type="email" name="email" placeholder="Email" />
+        <button type="button"> Cadastrar </button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span> {user.name}</span></p>
+            <p>Idade: <span> {user.age}  </span></p>
+            <p>Email: <span> {user.email}   </span></p>
+          </div>
+          <button>
+            <img src={Trash} alt="" width="20px" />
+          </button>
+        </div>
+      ))}
+
+
+    </div>
+  );
 }
 
-export default Home
+export default Home;
