102dba26d4d96c52c234e70cc8ee7875b5238cdb
ViniciusDev <viniciusdev10@gmail.com>
2025-01-06 09:00:41 -0300

    get data from frontend and save od databse

--- cadastro usuarios/src/pages/home/index.jsx
+++ cadastro usuarios/src/pages/home/index.jsx
@@ -1,13 +1,22 @@
 import "./style.css";
 import Trash from "../../assets/trash.png";
 import api  from "../../services/api";
-import { useEffect, useState } from "react"; // userefect vai executar sempre que a pagina abrir 
+import { useEffect, useState , useRef} from "react"; // userefect vai executar sempre que a pagina abrir 
 
+
+// react hock - useRef 
 
 function Home() {
 
   // aqui são declarados estados do react, USERS é minha variavel e declarando dessa forma eu consigo mudar e mostrar na tela
   const [users, setUsers] = useState([])
+
+
+  // dessa forma eu consigo salvar os dados do meu input aqui dentro
+  const inputName = useRef()
+  const inputaAge = useRef()
+  const inputEmail = useRef()
+
 
   async function getUsers(){
 
@@ -18,20 +27,37 @@ function Home() {
 
   }
 
+  async function createUsers(){
+
+    //const userFromApi = await api.get('/users')
+    
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputaAge.current.value,
+      email: inputEmail.current.value
+    })
+
+    getUsers()
+
+  }
+
+  //vai executar sempre que a pagina abrir 
   useEffect(() => {
 
     getUsers()
 
   },[])
 
+  
+
   return (
     <div className="containe">
       <form action="">
         <h1>Cadasto de usuarios</h1>
-        <input type="text" name="nome" placeholder="Nome" />
-        <input type="number" name="idade" placeholder="Numero" />
-        <input type="email" name="email" placeholder="Email" />
-        <button type="button"> Cadastrar </button>
+        <input type="text" name="nome" placeholder="Nome" ref={inputName} />
+        <input type="number" name="idade" placeholder="Numero" ref={inputaAge} />
+        <input type="email" name="email" placeholder="Email" ref={inputEmail} />
+        <button type="button" onClick={createUsers}> Cadastrar </button>
       </form>
 
       {users.map((user) => (
